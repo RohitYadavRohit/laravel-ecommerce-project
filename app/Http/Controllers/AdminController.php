f541da1357ba27d6dba6843fad49e5de19a15e22
RohitYadavRohit <140772308+RohitYadavRohit@users.noreply.github.com>
2026-05-10 12:27:56 +0530

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\category;
+
+class AdminController extends Controller
+{
+    public function addCategory()
+    {
+        return view('admin.addcategory');
+    }
+    public function postAddCategory(Request $request){
+        $category = new Category;
+        $category->category=$request->category;
+        $category->save();
+        return redirect()->back()->with('category_message','Category added successfully!');
+    }
+    public function viewCategory(){
+        $categories = Category::all();
+        return view('admin.viewcategory',compact('categories'));
+    }
+
+    public function deleteCategory($id){
+        $category=Category::findOrFail($id);
+
+        $category->delete();
+
+        return redirect()->back()->with('deletecategory_message','Deleted successfully!');
+
+    }
+}
